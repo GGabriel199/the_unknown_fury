33f33594fa58b34abc166e0d174bc14fa36f2ca8
GGabriel199 <gabrielflorencio@outlook.com>
2023-11-25 18:45:21 -0300

    dialogSystem and "theme"

---
+++ assets/scripts/dialogue/global_npc.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+var found_npc_item = false
+var given_npc_item = false
--- assets/scripts/world.gd
+++ assets/scripts/world.gd
@@ -4,16 +4,18 @@ var navigation_speed = 0.01
 var viewport_minimum = 200
 var view_resolution = Vector2()
 var viewport_resolution_variance = 1920
+var npc_in_range = false
+@onready var theme = $AudioStreamPlayer3D
 
 func _ready():
-	view_resolution.x = ($Camera_pivot/sky_camera.size /54) * viewport_resolution_variance
-	view_resolution.y = ($Camera_pivot/sky_camera.size /96) * viewport_resolution_variance
-	
+	view_resolution.x = ($Camera_pivot/sky_camera.size /5) * viewport_resolution_variance
+	view_resolution.y = ($Camera_pivot/sky_camera.size /14) * viewport_resolution_variance
+	theme.play()
 	get_tree().get_root().set_size(view_resolution)
 
 func _process(delta):
 	var forward = $Camera_pivot.transform.basis.z.normalized() * navigation_speed
-
+	
 	if Input.is_key_pressed(KEY_W):
 		$Camera_pivot.transform.origin += -forward
 	if Input.is_key_pressed(KEY_S):
@@ -28,6 +30,11 @@ func _process(delta):
 		
 	if Input.is_action_pressed("rotate_right"):
 		$Camera_pivot.rotation_degrees.y += 1
+
+func _physics_process(delta):
+	if npc_in_range == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			DialogueManager.show_example_dialogue_balloon(load("res://assets/scripts/dialogue/main.dialogue"), "startdialogue")
 
 func _input(event):
 	if Input.is_action_just_pressed("zoom_in"):
@@ -47,9 +54,18 @@ func _input(event):
 			_adjust_viewport_sized()
 
 func _adjust_viewport_sized():
-	view_resolution.x = ($Camera_pivot/sky_camera.size /54) * viewport_resolution_variance
-	view_resolution.y = ($Camera_pivot/sky_camera.size /96) * viewport_resolution_variance
+	view_resolution.x = ($Camera_pivot/sky_camera.size /5) * viewport_resolution_variance
+	view_resolution.y = ($Camera_pivot/sky_camera.size /14) * viewport_resolution_variance
 	
 	print (Vector2(view_resolution.x, view_resolution.y))
 	
 	get_tree().get_root().set_size(Vector2(view_resolution.x, view_resolution.y))
+
+func _on_detection_area_body_entered(body):
+	if body.has_method("npc_collide"):
+		npc_in_range = true
+		
+func _on_detection_area_body_exited(body):
+	if body.has_method("npc_collide"):
+		npc_in_range = false
+
